f9f6611a44d07cb9849e595645725e78408e679b
arturradiuk <arturradiuk@gmail.com>
2021-01-25 15:28:38 +0200

    merge with pawel  Changes to be committed: 	modified:   app/src/main/java/com/example/helply/menu/CreateAnnouncementActivity.java 	modified:   app/src/main/java/com/example/helply/popup/ListPopUpWindow.java 	modified:   app/src/main/res/layout/pop_up_window.xml 	modified:   app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/helply/popup/ListPopUpWindow.java
+++ app/src/main/java/com/example/helply/popup/ListPopUpWindow.java
@@ -3,11 +3,6 @@ package com.example.helply.popup;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -15,12 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 
 import com.example.helply.R;
 
 public class ListPopUpWindow extends Activity {
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +25,7 @@ public class ListPopUpWindow extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int) (width * .8), (int) (height * .6));
-        getWindow().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.white)));
+
         final Button saveListBtn = findViewById(R.id.save_shopping_list);
         final EditText shoppingList = findViewById(R.id.shopping_list_edit_text);
 
