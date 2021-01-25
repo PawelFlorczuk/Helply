41e953d996ffbcd28269320b2451e3ea234c3cab
arturradiuk <arturradiuk@gmail.com>
2021-01-25 15:25:58 +0200

    Changes to be committed: 	modified:   app/src/main/java/com/example/helply/menu/CreateAnnouncementActivity.java 	modified:   app/src/main/java/com/example/helply/popup/ListPopUpWindow.java 	modified:   app/src/main/res/layout/announcement_object.xml 	modified:   app/src/main/res/layout/pop_up_window.xml 	modified:   app/src/main/res/layout/sidebar_header.xml 	modified:   app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/helply/menu/CreateAnnouncementActivity.java
+++ app/src/main/java/com/example/helply/menu/CreateAnnouncementActivity.java
@@ -2,6 +2,8 @@ package com.example.helply.menu;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
