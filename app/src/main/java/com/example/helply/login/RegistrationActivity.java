b37fdad02fa53a033395c07c164db011fc2d936f
224293 <224293@edu.p.lodz.pl>
2021-01-26 13:29:47 +0100

    Refactoring

--- app/src/main/java/com/example/helply/login/RegistrationActivity.java
+++ app/src/main/java/com/example/helply/login/RegistrationActivity.java
@@ -71,11 +71,9 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         switch(view.getId()) {
             case R.id.signInBtn:
                 startActivity(new Intent(this, LoginActivity.class));
-                finish();
                 break;
             case R.id.registerBtn:
                 register();
-                finish();
                 break;
         }
     }
